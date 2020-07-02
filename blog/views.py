89ef451610e285e88ff83d45601c9c38fd9902fc
Michele-siqueira <michele.siqueira@b2wdigital.com>
2020-07-02 20:45:00 -0300

    Finalizando

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import PostForm
@@ -42,3 +43,16 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+=======
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import render
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+
+>>>>>>> 0d1ab830e251fbb42332c73859dcbbdd8aee3ccd
